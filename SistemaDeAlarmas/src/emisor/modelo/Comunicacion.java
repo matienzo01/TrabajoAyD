f197770bb4483ea748dc600fc31e4d7ca7721706
Matias <matiasgutierrez225@gmail.com>
2022-04-27 22:17:33 -0300

    Me falto guardar este archivo jeje

--- SistemaDeAlarmas/src/emisor/modelo/Comunicacion.java
+++ SistemaDeAlarmas/src/emisor/modelo/Comunicacion.java
@@ -1,5 +1,17 @@
 package emisor.modelo;
 
+import java.util.ArrayList;
+
 public class Comunicacion implements IEmergencia{
+	
+	private int puerto;
+	private ArrayList<Integer> ips;
+	
+
+	@Override
+	public void enviaNotificacion(Notificacion notificacion) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
